2b5ed62f3ccfa044f2d2d48665a312cbb3a54a81
terry_u16 <terry-u16@users.noreply.github.com>
2020-06-07 16:33:48 +0900

    Add CodeforcesUserResponse class

---
+++ AtCoderFollowees/AtCoderFollowees.Core/Data/CodeforcesUserResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoderFollowees.Core.Data
+{
+    public class CodeforcesUserResponse
+    {
+        public string? Status { get; set; }
+        public CodeforceUser[]? Result { get; set; }
+    }
+
+    public class CodeforceUser
+    {
+        public string? LastName { get; set; }
+        public string? Country { get; set; }
+        public int LastOnlineTimeSeconds { get; set; }
+        public string? City { get; set; }
+        public int Rating { get; set; }
+        public int FriendOfCount { get; set; }
+        public string? TitlePhoto { get; set; }
+        public string? Handle { get; set; }
+        public string? Avatar { get; set; }
+        public string? FirstName { get; set; }
+        public int Contribution { get; set; }
+        public string? Organization { get; set; }
+        public string? Rank { get; set; }
+        public int MaxRating { get; set; }
+        public int RegistrationTimeSeconds { get; set; }
+        public string? MaxRank { get; set; }
+    }
+}
