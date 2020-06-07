b5e6e3d5238e622b0c49ac9420d112bd4ef0e98b
terry_u16 <terry-u16@users.noreply.github.com>
2020-06-07 16:25:24 +0900

    Add AtCoderUserDbContext class

---
+++ AtCoderFollowees/AtCoderFollowees.Core/Data/AtCoderUserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using AtCoderFollowees.Core.Models;
+
+namespace AtCoderFollowees.Core.Data
+{
+    public class AtCoderUserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(user => user.TwitterID);   // 複数垢が同じscreen_nameを書いていると一意にならないので……
+        }
+    }
+}
